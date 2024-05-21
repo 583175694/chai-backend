e5c4badcdf46fc839d4af8ed7a862cd7fe263270
583175694 <583175694@qq.com>
2024-05-21 20:17:52 +0800

    fds

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
+import connectDBCCCC from "./db/index.js";
 import {app} from './app.js'
 dotenv.config({
     path: './.env'
@@ -8,7 +8,7 @@ dotenv.config({
 
 
 
-connectDB()
+connectDBCCCC()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
